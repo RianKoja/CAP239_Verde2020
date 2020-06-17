d56fc682055a08bd04c446b7847cc6945a70ad87
RianKoja <riankoja@gmail.com>
2020-06-16 23:11:29 -0300

    fixed getdata script, so it sieves out data from the series before the first day a country had 50+ new cases in a single day

--- src/tools/getdata.py
+++ src/tools/getdata.py
@@ -7,6 +7,7 @@
 # Standard imports:
 import pandas as pd
 import os
+import numpy as np
 
 
 # Use this function to yield the dataframe to be analyzed.
@@ -16,22 +17,30 @@ def acquire_data(country='United States', date_ini='2020-02-10', date_end='2020-
 
     if os.path.isfile(xls_filename):  # Read the csv file:
         print("Using local data for COVID-19 cases...")
-        df = pd.read_excel(xls_filename)
+        df = pd.read_excel(xls_filename, index_col=None)
     else:
         print("Using data online for COVID-19 cases...")
         url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
-        df = pd.read_csv(url, index_col=0)
+        df = pd.read_csv(url, index_col=None)
 
     # Separate country data:
     if country != 'all':
         df = df[df.location == country]
     # Separate date interval:
-    df = df[df.date.between(date_ini, date_end, inclusive=True)].reset_index()
+    df = df[df.date.between(date_ini, date_end, inclusive=True)]
     # Separate useful columns:
     wanted_columns = ['date', 'total_cases', 'new_cases', 'total_deaths', 'new_deaths']
     if country == 'all':
         wanted_columns.append('location')
     df = df[wanted_columns]
-    # TODO: Separate only after the first day with 50+ cases
+    # Remove incomplete data:
+    df.dropna(inplace=True)
+    # Reset index before next loop:
+    df.reset_index(inplace=True)
+    # Provide data only after the first day with 50+ new cases
+    for ii in range(0, len(df)):
+        if df.loc[ii, 'new_cases'] >= 50:
+            df = df.drop(range(0, max([0, ii])))
+            break
     # df.to_excel('test_df.xlsx') # Use to visualize selected data in excel.
     return df
