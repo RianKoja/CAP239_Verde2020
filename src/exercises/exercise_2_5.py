819e775c7acc0c9af7967f063c077b07f304b765
gioguarnieri <31107998+gioguarnieri@users.noreply.github.com>
2020-06-18 21:51:16 -0300

    Update exercise_2_5.py

--- src/exercises/exercise_2_5.py
+++ src/exercises/exercise_2_5.py
@@ -14,18 +14,18 @@ import matplotlib.pyplot as plt
 def run(countrylist, doc=None):
     countrylist.sort()
     for country in countrylist:
+        df = getdata.acquire_data(country, include_tests=True)
+        plt.figure()
+        plt.xlabel("New Daily Cases")
+        plt.grid("both")
+        plt.ylabel("New Daily Tests")
+        plt.title(country)
+        plt.plot(df['new_cases'], df['new_tests'], 'o', label="Data")
         try:
-            df = getdata.acquire_data(country, include_tests=True)
             m1 = [int(max(df['new_tests'])), int(max(df['new_cases']))]
-            plt.figure()
-            plt.xlabel("New Daily Cases")
-            plt.grid("both")
-            plt.ylabel("New Daily Tests")
-            plt.title(country)
-            plt.plot(df['new_cases'], df['new_tests'], 'o', label="Data")
             plt.plot(range(min(m1)), range(min(m1)), label="y=x")
         except:
-            print("No tests")
+            print("no tests\n")
         plt.draw()
         if doc is None:
             plt.show()
